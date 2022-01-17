f2db6abfe60f4deff164704127a3f2a663f906fc
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-01-17 16:24:53 -0500

    feat: custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { Page } from "@/components/Page";
+import type { NextPage } from "next";
+
+const Custom404: NextPage = () => {
+  return <Page title="404" description="Page not found" />;
+};
+
+export default Custom404;
